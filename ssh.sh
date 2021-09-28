49628e8f59a6882d0f0a5e8becc4c6d68fc1921b
fuckl0l <90489436+fuckl0l@users.noreply.github.com>
2021-09-28 20:05:06 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -10,4 +10,5 @@ sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen >/dev/null
 rm -f .ngrok.log
 ./ngrok authtoken 1yXJ100mAj0OpSLezldmQl02Dqf_54WFxVwTR6H9d5jaoQHpJ
 ./ngrok tcp 22 --log ".ngrok.log" &
+sleep 10
 echo $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh bambang@/" | sed "s/:/ -p /")
