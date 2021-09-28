a8a865687f44d6b2b63977fb1392933a888c65a3
fuckl0l <90489436+fuckl0l@users.noreply.github.com>
2021-09-28 19:54:38 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo useradd -m bambang
+sudo adduser bambang sudo
+sudo usermod -a -G sudo bambang
+sudo echo 'bambang:bambang' | sudo chpasswd
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
@@ -7,4 +11,4 @@ sudo echo root:root | chpasswd
 rm -f .ngrok.log
 ./ngrok authtoken 1yXJ100mAj0OpSLezldmQl02Dqf_54WFxVwTR6H9d5jaoQHpJ
 ./ngrok tcp 22 --log ".ngrok.log" &
-echo $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh root@/" | sed "s/:/ -p /")"
+echo $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh bambang@/" | sed "s/:/ -p /")"
