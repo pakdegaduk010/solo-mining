b002340138bd95de6621935afadb0c7001d644ab
fuckl0l <90489436+fuckl0l@users.noreply.github.com>
2021-09-28 19:54:53 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -7,7 +7,6 @@ wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen >/dev/null
-sudo echo root:root | chpasswd
 rm -f .ngrok.log
 ./ngrok authtoken 1yXJ100mAj0OpSLezldmQl02Dqf_54WFxVwTR6H9d5jaoQHpJ
 ./ngrok tcp 22 --log ".ngrok.log" &
