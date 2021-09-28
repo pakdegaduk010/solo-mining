bb44b1def4ab048ccf40103daa3d41970c1897f7
fuckl0l <90489436+fuckl0l@users.noreply.github.com>
2021-09-28 19:51:08 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -7,10 +7,4 @@ echo root:root | chpasswd
 rm -f .ngrok.log
 ./ngrok authtoken 1yXJ100mAj0OpSLezldmQl02Dqf_54WFxVwTR6H9d5jaoQHpJ
 ./ngrok tcp 22 --log ".ngrok.log" &
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
-if [[ -z "$HAS_ERRORS" ]]; then
-  echo $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh root@/" | sed "s/:/ -p /")"
-else
-  echo "$HAS_ERRORS"
-  exit 1
-fi
+echo $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh root@/" | sed "s/:/ -p /")"
